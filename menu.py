18d4509cb228234aa3f21dd60624e7808a2f55b3
SMA7921 <lalalala7921@gmail.com>
2024-12-24 11:59:13 +0100

    son

---
+++ menu.py
@@ -0,0 +1,15 @@
+'''
+displays resaturant menu
+'''
+import csv
+
+
+def menu_display():
+    ''' reads csv file'''
+    input_file = csv.DictReader(open("food.csv"),delimiter=";")
+    print("--- Menu ---")
+    for row in input_file:
+        print(f"{row["name"]}, {row["type"]}, {row["category"]}, {row["price"]}€")
+
+
+menu_display()
