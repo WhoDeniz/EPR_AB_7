eed33c609e6c97aff6e79cc4c309d49edbf969fd
SMA7921 <lalalala7921@gmail.com>
2024-12-25 12:53:43 +0100

    idk what to write

--- menu.py
+++ menu.py
@@ -4,16 +4,12 @@ displays resaturant menu
 import csv
 
 
-class Menu:
-
-    def __init__(self):
-        
-        def menu_display():
-            ''' reads csv file'''
-            input_file = csv.DictReader(open("food.csv"),delimiter=";")
-            print("--- Menu ---")
-            for row in input_file:
-                print(f"{row["name"]}, {row["type"]}, {row["category"]}, {row["price"]}€")
+def menu_display():
+    ''' reads csv file'''
+    input_file = csv.DictReader(open("food.csv"),delimiter=";")
+    print("--- Menu ---")
+    for row in input_file:
+        print(f"{row["name"]}, {row["type"]}, {row["category"]}, {row["price"]}€")
 
 
-menu_display()
+# TEST menu_display()
